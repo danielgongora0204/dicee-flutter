3612391a11b392352eebd3503e76afb560af06c9
danielgongora0204 <arez.gig.xd.g@gmail.com>
2022-09-21 18:12:22 -0700

    Added Dice Page (Widget Separation)
    Added new file, separating the logic between the host screen and the screen content.

---
+++ lib/dicePage.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class DicePage extends StatefulWidget {
+  String name = 'hello';
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = Random().nextInt(6) + 1;
+
+  int rightDiceNum = Random().nextInt(6) + 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNum = new Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNum = new Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
+class DicePage extends StatelessWidget {
+  int leftDiceNum = (new Random()).nextInt(6) + 1;
+  int rightDiceNum = (new Random()).nextInt(6) + 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                leftDiceNum = (new Random()).nextInt(6) + 1;
+              },
+              child: Image.asset('images/dice$leftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rightDiceNum = (new Random()).nextInt(6) + 1;
+              },
+              child: Image.asset('images/dice$rightDiceNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dicePage.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: SafeArea(child: DicePage()),
       ),
-    ),
-  );
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    );
   }
 }
